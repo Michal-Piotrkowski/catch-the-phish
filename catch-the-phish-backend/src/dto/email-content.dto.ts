22fb84ab6ef03448a01a1e3f720b37434a56d2c0
Michał Piotrkowski <michal.piotrkowski@protonmail.com>
2026-05-17 11:44:31 +0200

    feat: enable global validation pipe and improve validation error messages

--- catch-the-phish-backend/src/dto/email-content.dto.ts
+++ catch-the-phish-backend/src/dto/email-content.dto.ts
@@ -1,12 +1,18 @@
 import { IsString, MaxLength, MinLength } from 'class-validator';
 
 export default class EmailContentDto {
-  @IsString()
-  @MinLength(10)
+  @IsString({
+    message: 'Email content must be a string.',
+  })
+  @MinLength(10, {
+    message:
+      'Email content is too short. The minimum allowed length is 10 characters.',
+  })
   // 2k characters is ~512 tokens,
   // prevents exceeding the architectural limit of DistilBERT model
   @MaxLength(2000, {
-    message: 'Email content is too long. The maximum allowed length is 2000 characters.',
+    message:
+      'Email content is too long. The maximum allowed length is 2000 characters.',
   })
-  content: string;
+  content!: string;
 }
